5e668a6d486e92bb627f50bbd95be03a92e6ad1a
LordOfTheNugget <ncaccamo@live.ca>
2020-04-15 17:21:05 -0400

    Me

--- MiniPaint/StartupInfo.cs
+++ MiniPaint/StartupInfo.cs
@@ -16,5 +16,10 @@ namespace MiniPaint
         {
             InitializeComponent();
         }
+
+        private void JoinButton_Click(object sender, EventArgs e)
+        {
+          
+        }
     }
 }
